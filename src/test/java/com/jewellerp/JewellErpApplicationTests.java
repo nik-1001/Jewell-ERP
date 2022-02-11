f98f9fe11db99321e1db4dd1c5f1352840d9b2d9
nik-1001 <rohitnikam040@gmail.com>
2022-02-11 23:20:41 +0530

    This is Jewell ERP project

---
+++ src/test/java/com/jewellerp/JewellErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewellerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewellErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
